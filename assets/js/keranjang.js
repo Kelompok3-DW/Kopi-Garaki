19e2dc9bd7ca2f00ab9f4b2a56cac4be124daa04
MRizkiAG <mrizkiag1027@gmail.com>
2021-12-24 00:08:13 +0800

    perbaikan halaman keranjang
    urutan
    tambah js
    pindah metode pembayaran dari pembayaran ke keranjang

---
+++ assets/js/keranjang.js
@@ -0,0 +1,5 @@
+let dineIn = document.getElementById("dine-in");
+let takeAway = document.getElementById("take-away");
+let formNomorMeja = document.querySelector(".formNomorMeja");
+let formNoHP = document.querySelector(".formNoHP");
+let formAlamat = document.querySelector(".formAlamat");
