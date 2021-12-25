01f6d6c7d0d32864d297d43de960fb85461e25cf
MRizkiAG <mrizkiag1027@gmail.com>
2021-12-26 07:31:51 +0800

    perbaikan detail pemesan di halaman keranjang

--- assets/js/keranjang.js
+++ assets/js/keranjang.js
@@ -3,3 +3,17 @@ let takeAway = document.getElementById("take-away");
 let formNomorMeja = document.querySelector(".formNomorMeja");
 let formNoHP = document.querySelector(".formNoHP");
 let formAlamat = document.querySelector(".formAlamat");
+
+document.querySelector("#disabledSelect").onchange = (e) => {
+	if(e.target.value === 'take-away'){
+		// aksi bila take-away
+        formNomorMeja.className = "item-hide";
+        formNoHP.className = "item-visible";
+        formAlamat.className = "item-visible";
+	} else {
+		// aksi bila dine-in
+        formNomorMeja.className = "item-visible";
+        formNoHP.className = "item-hide";
+        formAlamat.className = "item-hide";
+	}
+}
